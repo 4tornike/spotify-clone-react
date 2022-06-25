03521a7e63632fc655c8270cc36e3507162398ac
tornike_44 <4tornike@gmail.com>
2022-06-25 19:31:36 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import React,{ useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer'
 
 function App() {
+
+  const spotify = new SpotifyWebApi()
+
+  const [{ user, token },dispatch] = useDataLayerValue()
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ""
+
+    const _token = hash.access_token
+
+    if(_token) {
+
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token
+      })
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+
+      }).catch(console.error())
+
+      spotify.getUserPlaylists().then(playlists => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists: playlists
+        })
+      })
+
+      spotify.getPlaylist('6YFFeM9R9QxZh4M65Ih73Y').then(res => {
+        dispatch({
+          type: 'SET_DISCOVERWEEKLY',
+          discover_weekly: res
+        })
+      })
+
+    }
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/"
+
+const client_id = "168b94d31e7d4f37a8d643c48848e5ac"
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state"
+]
+
+//  #access_token=BQDN-WmB3bke58uZARhw7BVOZNkNfp7B_3Z1Ve4hIxRkwN0-12gRLevP179t0rXFWvwdGizCMXlvLidgdCnXGJTCSauuaBLaYEwFEohIg_DCblQoV4ueVqMqJh_9sHaf1u5GZp1g4aToWCb0HzmLOzNenAYeFdrXWvd9ZNdfjsjI3uV49KinOzgYaPZwshAHge_ym3q-GgO6zqVyGQ&token_type=Bearer&expires_in=3600
+export const getTokenFromUrl = () => {
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial,item) => {
+        let parts = item.split('=')
+        initial[parts[0]] = decodeURIComponent(parts[1])
+        return initial
+    },{})
+}
+
+export const loginUrl = `${authEndPoint}?client_id=${client_id}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
